cdeb5b6da2866ccd9a8697671da170000788b911
Carina Cristina Teixeira De Souza <carinacristina1809@gmail.com>
2026-03-10 12:05:44 -0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,10 @@
+//====================================================
+//  NOSSA API DE CACHORROS 
+//  Feita com Node.js + Express
+
+
+//  Agora as fotos NÃO são mais baixadas automaticamente.
+//  Elas devem existir manualmente na pasta:
+
+//data/foto/
+//=====================================================
